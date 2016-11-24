fbad48488f3512d5b12bac4c792c07665c2ffa06
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-24 13:48:07 -0500

    auth

--- bot.py
+++ bot.py
@@ -1,8 +1,4 @@
 import tweepy
-
-config = configparser.ConfigParser()
-config = config.read('config.ini')
- = config['twitter']
-
-auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], config['CONSUMER_SECRET'])
-auth.set_access_token(config['ACCESS_TOKEN'], config['ACCESS_SECRET'])
+from credentials import *
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
