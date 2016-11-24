d4e88781d61ba325dc55fb103b9fe4c84d9d29aa
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-24 13:30:32 -0500

    basic steps'

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
-import tweepy
+import tweepy, configparser
+
+
+config = configparser.ConfigParser()
+config = config.read('config.ini')
+config = config['twitter']
+
+auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], ['CONSUMER_SECRET'])
+auth.set_access_token(config['ACCESS_TOKEN'],['ACCESS_SECRET'])
