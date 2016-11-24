4b8a0e09278f596506c0e9e88e853ffc60c5d8c0
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-24 14:43:13 -0500

    more config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: bot.py
+worker: source/bot.py
---
+++ source/bot.py
@@ -0,0 +1,10 @@
+import tweepy, json, os
+import credentials
+
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
+bot = tweepy.API(auth)
+
+bot.update_status('www.getaderal.com')
+
+print "success"
