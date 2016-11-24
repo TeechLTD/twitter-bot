f184a3f203c4730fb02cc94bbc3de8a76be03990
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-24 14:14:39 -0500

    heroku config

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
 import tweepy
+import json
 from credentials import *
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+bot = tweepy.API(auth)
+
+bot.update_status('ready to rock')
+
+
+print "success"
