29623417d20ecea798c6bc0e970da9258c0272e3
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-24 13:45:40 -0500

    new implementation

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-import tweepy, configparser
-
+import tweepy
 
 config = configparser.ConfigParser()
 config = config.read('config.ini')
-config = config['twitter']
+ = config['twitter']
 
-auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], ['CONSUMER_SECRET'])
-auth.set_access_token(config['ACCESS_TOKEN'],['ACCESS_SECRET'])
+auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], config['CONSUMER_SECRET'])
+auth.set_access_token(config['ACCESS_TOKEN'], config['ACCESS_SECRET'])
