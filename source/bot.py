450ee65e3f01760e7176f50043ba84dca2d59b07
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-24 22:39:31 -0500

    rate limitation added

--- source/bot.py
+++ source/bot.py
@@ -1,29 +1,23 @@
-import tweepy, json, os
+# Twitter limits : 3200 tweets, 200 at a time  (15min interval)
+
+import tweepy, json, os, datetime
 
 if os.environ["bot_env"] == 'development':
      import credentials
 
 auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
 auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
-bot = tweepy.API(auth)
+bot = tweepy.API(auth, monitor_rate_limit=True, wait_on_rate_limit=True)
 
-def duplicate():
-
-
-    return;
 
 def tweet():
     from tweets import tweet_list
-
-    for tweet in tweet_list:
-        bot.upda
+    #method to tweet from premade list
     return;
 
-
-
-tweet()
-
-
+def search(parameter):
+    #fetch user list by parameters
+    return;
 
 
 print "done"
