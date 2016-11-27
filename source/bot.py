ad5975acb60d9108d05c6c0c9efa0eef190a9b20
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-27 18:07:32 -0500

    bot v1

--- source/bot.py
+++ source/bot.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import tweepy, os, time
 
 if os.environ["bot_env"] == 'development':
@@ -19,74 +17,81 @@ class StreamListener(tweepy.StreamListener):
         elif status_code == 429:
             return False
         else:
-            wait_a_minute
+            time.sleep(90)
             return True
 
 def display_tweet(tweet):
     print("Tweet Message : " + tweet.text)
-    print("Tweet id : " + str(tweet.id))
+    print("Tweet id : " + tweet.id.encode('utf-8'))
     print("Tweet user : " + tweet.user.name)
-    print("Tweet user id : " + str(tweet.user.id) + "\n")
-    friendship = bot.show_friendship(target_id=tweet.user.id)
-    print friendship[1].followed_by
+    print("Tweet user id : " + (tweet.user.id).encode('utf-8') + "\n")
+
 
 def act_on_tweet(tweet):
     tweet_id = tweet.id
     user = tweet.user
 
-    frienship = bot.show_friendship(target_id=tweet.user.id)
-    friends = friendship[1].followed_by
+    follower = follow_us(user)
+    time.sleep(90)
 
     try:
-        bot.create_favorite(tweet_id)
-        print("favorited a tweet by: " + str(user.name) + "\n")
-    except:
-        print("favoriting a tweet by: " + str(user.name) + "failed! \n" )
+        api.create_favorite(tweet_id)
+        print("favorited a tweet by: " + user.name)
+        print(tweet.text + "\n")
+    except Exception as e:
+        print("favoriting a tweet by: " + user.name + " failed! \n" )
+        print(e + "\n")
         pass
 
-    wait_a_minute
+    time.sleep(90)
 
-    # follow if user does not follow us
-    if friends:
-        if "@aderalv2" in tweet.text:
-            try:
-                bot.retweet(tweet_id)
-                reply(tweet_id, user.name)
-            except:
-                print("replying to:" + str(user.name) + "failed! \n" )
-    else:
-        bot.create_friendship(user.id, bot_id)
+    if "@aderalv2" in tweet.text:
+        try:
+            api.retweet(tweet_id)
+            reply(tweet_id, user.name)
+        except Exception as e:
+            print("replying to: " + user.name + " failed!" )
+            print(e + "\n")
+            
+    if not follower:
+        try:
+            api.create_friendship(user.id, api.me)
+            print("Requested to follow :" + user.name + "\n")
+        except Exception as e:
+            print("request to follow: " + user.name + " failed!" )
+            print(e + "\n")
 
-    wait_a_minute
+    time.sleep(90)
 
 def reply(tweet_id, user_name):
     reply = "@%s, we are listening. Join our community today to boost your potential." % (user_name)
     try:
-        bot.update_status(reply, tweet_id)
-        print("Retweeted and replied to: " + str(user.name) + "\n")
-        wait_a_minute
+        api.update_status(reply, tweet_id)
+        print("Retweeted and replied to: " + user.name + "\n")
+        time.sleep(90)
     except:
-        print("reply to: " + str(user.name) + "failed! \n" )
+        print("reply to: " + user.name + " failed! \n" )
         pass
 
+def follow_us(user):
+    friendship = api.show_friendship(target_id=user.id)
+    friends = friendship[1].followed_by
+    return friends
 
 def direct_message(user):
-    message = "@%s, welcome to our world. То learn more about us and check on availabilities - follow the link in our bio." % (user.name)
+    message = "@%s, welcome to our world. To learn more about our exclusive product and reserve your spot in the waitlist - follow the link in our bio." % (user_name)
     try:
-        bot.send_direct_message(user.id)
-        print("sent a direct message to: " + str(user.name) + "\n")
+        api.send_direct_message(user.id)
+        print("sent a direct message to: " + user.name + "\n")
     except:
-        print("direct message to: " + str(user.name) + "failed! \n" )
+        print("direct message to: " + user.name + " failed! \n" )
         pass
-    wait_a_minute
+    time.sleep(90)
 
 def unfollow(user_list):
     # TODO
     return None
 
-# prevent excessive spamming
-def wait_a_minute():
-    sleep(20)
 
 if __name__ == '__main__':
 
@@ -94,15 +99,11 @@ if __name__ == '__main__':
     auth.secure = True
     auth.set_access_token(keys['ACCESS_TOKEN'], keys['ACCESS_SECRET'])
 
-    bot = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True,  retry_count=10, retry_delay=5, retry_errors=5)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True,  retry_count=10, retry_delay=5, retry_errors=5)
     streamListener = StreamListener()
-    myStream = tweepy.Stream(auth=bot.auth, listener=streamListener)
+    myStream = tweepy.Stream(auth=api.auth, listener=streamListener)
 
-    bot_id = bot.get_user("aderalv2").id
-    # TODO: add a cursor here
-    bot_followers = bot.followers(bot_id)
-
-    search_terms = ["adderal", "aderal", "adderral", "I need adderall", '@aderalv2', 'getaderal.com', 'education']
+    search_terms = ["adderal", "aderal", "adderral", "I need adderall", '@aderalv2', 'getaderal.com', 'need a tutor', 'A-levels']
     myStream.filter(languages=["en"], track=search_terms, async=True)
 
 print "Fetching..... \n"
