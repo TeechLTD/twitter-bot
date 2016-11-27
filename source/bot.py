24c508f462e1495e081770bf92c6fdf00c70f075
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-26 22:51:22 -0500

    stream added

--- source/bot.py
+++ source/bot.py
@@ -3,16 +3,28 @@ import tweepy, os, json
 if os.environ["bot_env"] == 'development':
      import credentials
 
-auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
-bot = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True,  retry_count=10, retry_delay=5, retry_errors=5)
+class StreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        get_tweet(status)
 
 
 
+def get_tweet(tweet):
+    print("Tweet Message : " + tweet.text + "\n")
 
-# process(search_results)
 
 
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+    auth.secure = True
+    auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
 
+    bot = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True,  retry_count=10, retry_delay=5, retry_errors=5)
+
+    streamListener  = StreamListener()
+    myStream = tweepy.Stream(auth=bot.auth, listener=streamListener)
+
+    myStream.filter(track=['adderall'], async=True)
 
 print "\n done"
