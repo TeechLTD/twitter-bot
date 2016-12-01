bf6451f0a62553135d0e166a3dcaf916be8cac95
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-11-30 23:27:00 -0500

    reorganisation

--- lib/processor.py
+++ lib/processor.py
@@ -1,4 +1,81 @@
+def display_tweet(tweet):
+    """displays useful information"""
 
-class Processor():
+    print("Tweet Message : " + tweet.text)
+    print("Tweet id : " + str(tweet.id))
+    print("Tweet user name : " + tweet.user.name)
+    print("Tweet user handle : " + tweet.user.screen_name)
+    print("Tweet user id : " + str(tweet.user.id) + "\n")
 
-   def __init__(self):
+def act_on(tweet, api):
+    """main logic"""
+
+    tweet_id = tweet.id
+    user = tweet.user
+    follower = follow_us(user)
+    mention = "@aderalv2" in tweet.text
+
+    favorite(tweet)
+
+    print("sleeping... " + "\n")
+    time.sleep(45)
+
+    if mention:
+        reply_and_retweet(tweet_id, user.screen_name)
+
+    if not follower:
+        follow(user.id, api.me)
+
+def favorite(tweet_id):
+    """tries to favorite a tweet"""
+
+    print("3")
+    try:
+        api.create_favorite(tweet_id)
+        print("favorited a tweet by: " + user.name)
+        print(tweet.text + "\n")
+    except Exception as e:
+        print("favoriting a tweet by: " + user.name + " failed! \n" )
+        print(e)
+        print("\n")
+        pass
+
+def follow(user_id, bot):
+    try:
+        api.create_friendship(user_id, bot)
+        print("Requested to follow :" + user.name + "\n")
+    except Exception as e:
+        print("request to follow: " + user.name + " failed!" )
+        print(e)
+        print "\n"
+
+def reply(tweet_id, user_handle):
+    """replies to tweets"""
+
+    reply = "@%s, we are listening. Join our community today to boost your potential." % (user_handle)
+    try:
+        api.retweet(tweet_id)
+        api.update_status(reply, tweet_id)
+        print("Retweeted and replied to: " + user_handle + "\n")
+    except:
+        print("reply to: " + user_handle + " failed! \n" )
+        pass
+
+def follow_us(user):
+    """checks if a user follows us or not"""
+
+    friendship = api.show_friendship(target_id=user.id)
+    friends = friendship[1].followed_by
+    return friends
+
+def direct_message(user):
+    """sends a direct message to a user"""
+
+    message = "@%s, welcome to our world. To learn more about our exclusive product and reserve your spot in the waitlist - follow the link in our bio." % (user_name)
+    try:
+        api.send_direct_message(user.id)
+        print("sent a direct message to: " + user.name + "\n")
+    except:
+        print("direct message to: " + user.name + " failed! \n" )
+        pass
+    time.sleep(90)
--- lib/streamListener.py
+++ lib/streamListener.py
@@ -1,12 +1,15 @@
 import tweepy
+from processor import act_on, display_tweet
 
 class StreamListener(tweepy.StreamListener):
 
-    def on_status(self, status):
-            # display_tweet(status)
-            print "Listening..... \n"
-            act_on_tweet(status)
+    def __init__(self, api_instance):
+        print("1")
+        self.api = api_instance
 
+    def on_status(self, status):
+            print("2")
+            act_on(status, self.api)
 
     def on_error(self, status):
         if status_code is 420:
@@ -28,72 +31,3 @@ class StreamListener(tweepy.StreamListener):
             print(status)
             time.sleep(120)
             return False
-
-def display_tweet(tweet):
-    print("Tweet Message : " + tweet.text)
-    print("Tweet id : " + str(tweet.id))
-    print("Tweet user name : " + tweet.user.name)
-    print("Tweet user handle : " + tweet.user.screen_name)
-    print("Tweet user id : " + str(tweet.user.id) + "\n")
-
-
-def act_on_tweet(tweet):
-    tweet_id = tweet.id
-    user = tweet.user
-
-    follower = follow_us(user)
-
-    try:
-        api.create_favorite(tweet_id)
-        print("favorited a tweet by: " + user.name)
-        print(tweet.text + "\n")
-    except Exception as e:
-        print("favoriting a tweet by: " + user.name + " failed! \n" )
-        print(e)
-        print("\n")
-        pass
-
-    print("sleeping... " + "\n")
-    time.sleep(45)
-
-    if "@aderalv2" in tweet.text:
-        try:
-            api.retweet(tweet_id)
-            reply(tweet_id, user.screen_name)
-        except Exception as e:
-            print("replying to: " + user.name + " failed!" )
-            print(e)
-            print "\n"
-
-    if not follower:
-        try:
-            api.create_friendship(user.id, api.me)
-            print("Requested to follow :" + user.name + "\n")
-        except Exception as e:
-            print("request to follow: " + user.name + " failed!" )
-            print(e)
-            print "\n"
-
-def reply(tweet_id, user_handle):
-    reply = "@%s, we are listening. Join our community today to boost your potential." % (user_handle)
-    try:
-        api.update_status(reply, tweet_id)
-        print("Retweeted and replied to: " + user_handle + "\n")
-    except:
-        print("reply to: " + user_handle + " failed! \n" )
-        pass
-
-def follow_us(user):
-    friendship = api.show_friendship(target_id=user.id)
-    friends = friendship[1].followed_by
-    return friends
-
-def direct_message(user):
-    message = "@%s, welcome to our world. To learn more about our exclusive product and reserve your spot in the waitlist - follow the link in our bio." % (user_name)
-    try:
-        api.send_direct_message(user.id)
-        print("sent a direct message to: " + user.name + "\n")
-    except:
-        print("direct message to: " + user.name + " failed! \n" )
-        pass
-    time.sleep(90)
