6b9be9e16ec5cb6901c73810eecdb80e81eaf86f
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-12-03 19:05:48 -0500

    username replacement

--- utilities/follower.py
+++ utilities/follower.py
@@ -19,7 +19,6 @@ def get_parameters():
 def follow(users):
 
     success_count, error_count = 0, 0
-
     for user_id in tqdm(target_users, desc="sending follow requests"):
             try:
                 api.create_friendship(user_id, api.me)
@@ -32,23 +31,37 @@ def follow(users):
 
 def fetch_users(target_account, upper_bound):
     target_users = []
+    friends = []
+
+    for friend in tweepy.Cursor(api.friends_ids, screen_name).items():
+        friends.append(friend)
 
     for follower in tweepy.Cursor(api.followers_ids, target_account).items(upper_bound):
-        target_users.append(follower)
+        #look for follower upper_bound + 1 if we already follow current one
+        if follower in friends:
+            upper_bound += 1
+        else:
+            target_users.append(follower)
 
     return target_users
+
 
 if __name__ == '__main__':
     auth = tweepy.OAuthHandler(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'])
     auth.set_access_token(keys['ACCESS_TOKEN'], keys['ACCESS_SECRET'])
-    screen_name = 'aderalv2'
+    screen_name = 'TeechGlobal'
 
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True,  retry_count=10, retry_delay=5, retry_errors=5, timeout=60)
 
     target_account, upper_bound = get_parameters()
 
     target_users = fetch_users(target_account, upper_bound)
+
+    if len(target_users) is 0:
+        print("We already follow all their followers")
+        sys.exit()
+
     print("Loaded " + str(len(target_users)) + " users into the pipeline \n")
-    sleep(1)
+    sleep(3)
 
     follow(target_users)
